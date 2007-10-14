9c71c6e4075a375fab0487e625a2dc8ccd874f95
apavelm <apavelm@ce0af37e-8b3b-0410-b313-1d31aded5f2d>
2007-10-14 07:45:45 +0000

    fix

--- src/mdi_filelist.h
+++ src/mdi_filelist.h
@@ -3,13 +3,13 @@
 
 #include "mdi_c.h"
 
+#include "ui_filelistdialog.h"
+
 class FileListDlg : public MdiChild, private Ui::DialogFileList
 {
 	Q_OBJECT
 public:
 	FileListDlg(QWidget *parent);
 };
-
-#include "ui_filelistdialog.h"
 
 #endif // __MDI_FILELIST_H__
